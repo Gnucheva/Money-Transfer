b5ba2c22c8bcc4b8853200303a93d6e217fb20db
Anastasia <n41111215@mail.ru>
2020-04-30 16:08:51 +0300

    Добавила код

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args) {
+        int bill = 2000000000;
+        int transfer = 500000000;
+        int total = bill + transfer;
+        System.out.println(total);
+    }
+
+}
